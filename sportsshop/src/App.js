03512bd1708e36a8e099fea361130093f8c8b78d
IsmaelAlomari <ismaelalomari994@gmail.com>
2021-06-01 14:44:37 +0300

    reacttt

--- sportsshop/src/App.js
+++ sportsshop/src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import products from "./products";
+import "./App.css";
 
 function App() {
+
+  const product11 = products.map((product) => (
+   <div className ="productImages">
+      <img className="sport-Images" src={product.image} />
+      <div className="styleOfWords">{product.name}</div>
+      <div>{product.price}</div>
+      
+    </div>
+  ));
+
   return (
     <div>
       <header className= "GalaxySports">Galaxy Sports</header>
@@ -10,7 +21,9 @@ practice your favorite sport
 </p>
 
 <img className= "imagesport" src = "https://lh3.googleusercontent.com/2BBO1xZFq-dUB4igoS0nsMeHW4-v2smnvauhXrnCwer0PM6LmlgWHoLeUHHZjfLl8nfwRQNreYGcognRyvLQi92Xuw=w1000" />
-
+<div className="store">Products in Store </div>
+        <div className="productsList">{product11}</div>
+    
     </div>
   );
 }
---
+++ sportsshop/src/products.js
@@ -0,0 +1,15 @@
+
+
+const products = [{
+    name: "Balls",
+    price: 3,
+    image: "https://img.etimg.com/thumb/msid-64827637,width-640,resizemode-4,imgsize-340584/balls-with-tech.jpg"
+    
+},
+{
+    name: "T-shirt",
+    price: 10,
+    image: "https://ae01.alicdn.com/kf/H1236e370b9d7465c9db84d4a40ca6020x/2020-Badminton-Shirts-Sports-t-shirts-Men-Women-Quick-Dry-Breathable-Table-Tennis-T-shirts-Running.jpg"
+}]
+
+export default products
