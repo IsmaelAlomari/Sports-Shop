d5f547538d283be50cba9565ca1eaf70cce42af9
IsmaelAlomari <ismaelalomari994@gmail.com>
2021-06-01 11:28:14 +0300

    react

---
+++ sportsshop/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className= "GalaxySports">Galaxy Sports</header>
+      <p className= "description"> Get your own special sports equipment from Galaxy sports to
+practice your favorite sport
+</p>
+<i class="icon"> </i>
+<img src = "https://lh3.googleusercontent.com/2BBO1xZFq-dUB4igoS0nsMeHW4-v2smnvauhXrnCwer0PM6LmlgWHoLeUHHZjfLl8nfwRQNreYGcognRyvLQi92Xuw=w1000" />
+
+    </div>
+  );
+}
+
+export default App;
